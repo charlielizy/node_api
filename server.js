9580571e8f6ea4ad1753cece41995073810250ee
Charlie Li <39077299+charlielizy@users.noreply.github.com>
2020-09-09 20:21:21 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const restify = require('restify');
+const config = require('./app/config/config');
+const corsMiddleware = require('restify-cors-middleware');
+var routes = require('./app/routes/index');
+
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+// initial server
+routes.init(server);
+
+//logging
+server.pre(function (request, response, next) {
+  request.log.info({ req: request });
+  return next();
+});
+
+//use CORS
+const cors = corsMiddleware({
+  origins: ['*'],
+  credentials: false,
+  headers: ['Authorization', 'X-Sitename'],
+  allow_headers: ['Authorization', 'X-Sitename'],
+  methods: ['GET', 'POST', 'OPTIONS', 'HEAD', 'PUT', 'DELETE']
+})
+server.pre(cors.preflight);
+server.use(cors.actual);
+
+// // not found error
+server.on('NotFound', function (request, response, error) {
+  response.status(error.status || 500);
+  response.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+server.on('error', function (request, response, error) {
+  response.status(error.status || 500);
+  response.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log('Listening on: ' + process.env.PORT);
+  console.log('Current environment: ' + config.environment);
+});
+
